2f4fcaec24f10a1bda29dd0e91b85fb115ff4ee4
Ayush SV Kadam <kadamayush3619@gmail.com>
2026-06-29 19:54:08 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+
+bool isAnagram(string s, string t) {
+    
+    if( s.size() != t.size() ){
+        return false;
+    }
+
+    multiset< char > ss( s.begin(), s.end() );
+    multiset< char > tt( t.begin(), t.end() );
+
+    return ss==tt; 
+}
+
+};
